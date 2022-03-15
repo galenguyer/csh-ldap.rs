922345cdfd646049c349cfb24749eb2792bef83d
Galen Guyer <galen@galenguyer.com>
2022-03-14 22:23:02 -0400

    Initial commit for Joe

---
+++ src/client.rs
@@ -0,0 +1,169 @@
+use ldap3::{drive, Ldap, LdapConnAsync, Mod, SearchEntry};
+use rand::prelude::SliceRandom;
+use rand::SeedableRng;
+use std::collections::HashSet;
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    AsyncResolver,
+};
+
+use super::user::{LdapUser, LdapUserChangeSet};
+
+const SEARCH_ATTRS: [&str; 9] = [
+    "cn",
+    "dn",
+    "uid",
+    "memberOf",
+    "krbPrincipalName",
+    "mail",
+    "mobile",
+    "ibutton",
+    "drinkBalance",
+];
+
+#[derive(Clone)]
+pub struct LdapClient {
+    ldap: Ldap,
+}
+
+impl LdapClient {
+    pub async fn new(bind_dn: &str, bind_pw: &str) -> Self {
+        let servers = get_ldap_servers().await;
+        let (conn, mut ldap) = LdapConnAsync::new(
+            servers
+                .choose(&mut rand::rngs::StdRng::from_entropy())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+        drive!(conn);
+
+        ldap.simple_bind(bind_dn, bind_pw).await.unwrap();
+
+        LdapClient { ldap }
+    }
+
+    pub async fn search_users(&mut self, query: &str) -> Vec<LdapUser> {
+        self.ldap.with_timeout(std::time::Duration::from_secs(5));
+        let (results, _result) = self
+            .ldap
+            .search(
+                "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu",
+                ldap3::Scope::Subtree,
+                &format!("(|(uid=*{query}*)(cn=*{query}*))"),
+                SEARCH_ATTRS,
+            )
+            .await
+            .unwrap()
+            .success()
+            .unwrap();
+
+        results
+            .iter()
+            .map(|result| {
+                let user = SearchEntry::construct(result.to_owned());
+                LdapUser::from_entry(&user)
+            })
+            .collect()
+    }
+
+    pub async fn _do_not_use_get_all_users(&mut self) -> Vec<LdapUser> {
+        let (results, _result) = self
+            .ldap
+            .search(
+                "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu",
+                ldap3::Scope::Subtree,
+                "(objectClass=cshMember)",
+                SEARCH_ATTRS,
+            )
+            .await
+            .unwrap()
+            .success()
+            .unwrap();
+
+        results
+            .iter()
+            .map(|result| {
+                let user = SearchEntry::construct(result.to_owned());
+                LdapUser::from_entry(&user)
+            })
+            .collect()
+    }
+
+    pub async fn get_user(&mut self, uid: &str) -> Option<LdapUser> {
+        self.ldap.with_timeout(std::time::Duration::from_secs(5));
+        let (results, _result) = self
+            .ldap
+            .search(
+                "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu",
+                ldap3::Scope::Subtree,
+                &format!("uid={uid}"),
+                SEARCH_ATTRS,
+            )
+            .await
+            .unwrap()
+            .success()
+            .unwrap();
+
+        if results.len() == 1 {
+            let user = SearchEntry::construct(results.get(0).unwrap().to_owned());
+            Some(LdapUser::from_entry(&user))
+        } else {
+            None
+        }
+    }
+
+    pub async fn get_user_by_ibutton(&mut self, ibutton: &str) -> Option<LdapUser> {
+        self.ldap.with_timeout(std::time::Duration::from_secs(5));
+        let (results, _result) = self
+            .ldap
+            .search(
+                "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu",
+                ldap3::Scope::Subtree,
+                &format!("ibutton={ibutton}"),
+                SEARCH_ATTRS,
+            )
+            .await
+            .unwrap()
+            .success()
+            .unwrap();
+
+        if results.len() == 1 {
+            let user = SearchEntry::construct(results.get(0).unwrap().to_owned());
+            Some(LdapUser::from_entry(&user))
+        } else {
+            None
+        }
+    }
+
+    pub async fn update_user(&mut self, change_set: &LdapUserChangeSet) {
+        let mut changes = Vec::new();
+        if change_set.drinkBalance.is_some() {
+            changes.push(Mod::Replace(
+                String::from("drinkBalance"),
+                HashSet::from([change_set.drinkBalance.unwrap().to_string()]),
+            ))
+        }
+        match self.ldap.modify(&change_set.dn, changes).await {
+            Ok(_) => {}
+            Err(e) => eprintln!("{:#?}", e),
+        }
+    }
+}
+
+async fn get_ldap_servers() -> Vec<String> {
+    let resolver =
+        AsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    let response = resolver.srv_lookup("_ldap._tcp.csh.rit.edu").await.unwrap();
+
+    // TODO: Make sure servers are working
+    response
+        .iter()
+        .map(|record| {
+            format!(
+                "ldaps://{}",
+                record.target().to_string().trim_end_matches('.')
+            )
+        })
+        .collect()
+}
---
+++ src/user.rs
@@ -0,0 +1,82 @@
+use lazy_static::lazy_static;
+use ldap3::SearchEntry;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::str::FromStr;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[allow(non_snake_case)]
+pub struct LdapUser {
+    pub dn: String,
+    pub cn: String,
+    pub uid: String,
+    pub groups: Vec<String>,
+    pub krbPrincipalName: String,
+    pub mail: Vec<String>,
+    pub mobile: Vec<String>,
+    pub drinkBalance: Option<i64>,
+    pub ibutton: Vec<String>,
+}
+
+impl LdapUser {
+    pub fn from_entry(entry: &SearchEntry) -> Self {
+        let user_attrs = &entry.attrs;
+        LdapUser {
+            dn: entry.dn.clone(),
+            cn: get_one(user_attrs, "cn").unwrap(),
+            uid: get_one(user_attrs, "uid").unwrap(),
+            groups: get_groups(get_vec(user_attrs, "memberOf")),
+            krbPrincipalName: get_one(user_attrs, "krbPrincipalName").unwrap(),
+            mail: get_vec(user_attrs, "mail"),
+            mobile: get_vec(user_attrs, "mobile"),
+            ibutton: get_vec(user_attrs, "ibutton"),
+            drinkBalance: get_one(user_attrs, "drinkBalance"),
+        }
+    }
+}
+
+fn get_one<T>(entry: &HashMap<String, Vec<String>>, field: &str) -> Option<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    entry
+        .get(field)
+        .map(|f| f.get(0).unwrap().parse::<T>().unwrap())
+}
+
+fn get_vec<T>(entry: &HashMap<String, Vec<String>>, field: &str) -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    match entry.get(field) {
+        Some(v) => v.iter().map(|f| f.parse::<T>().unwrap()).collect(),
+        None => vec![],
+    }
+}
+
+pub fn get_groups(member_of: Vec<String>) -> Vec<String> {
+    lazy_static! {
+        static ref GROUP_REGEX: Regex =
+            Regex::new(r"cn=(?P<name>\w+),cn=groups,cn=accounts,dc=csh,dc=rit,dc=edu").unwrap();
+    }
+    member_of
+        .iter()
+        .filter_map(|group| {
+            GROUP_REGEX
+                .captures(group)
+                .map(|cap| cap["name"].to_owned())
+        })
+        .collect()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[allow(non_snake_case)]
+pub struct LdapUserChangeSet {
+    pub dn: String,
+    pub drinkBalance: Option<i64>,
+    pub ibutton: Option<Vec<String>>,
+}
